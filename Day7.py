46a80c1fcf17af6950340cef9b8f214bd239d2aa
Jungmoo Yoon <alexjm@naver.com>
2023-01-20 10:42:26 +0900

    class pokemon inheritance

--- Day7.py
+++ Day7.py
@@ -1,17 +1,43 @@
-#Generator: Yeild 는 함수가 멈추지 않고 계속 실행된다. 장단점 : generator는 메모리 절약 가능, 저장을 안함(last 값만 기억하고 나머지는 버린다.
-#단점: 잠깐 실행되느 마는 느낌이라 메모리 저장이 안됨
-#상속의 장점 : 코드의 중복을 피할 수 있다.
+# class
 
-#override는 부모 클라스에서 정의된 함수를 가져와서 다시 정의하는 것을 의미한다.
+class Pokemon:
+    def __init__(self, owner, skills):
+        self.owner = owner
+        self.skills = skills.split('/')
+        print(f"포켓몬 생성 : ", end=" ")
 
-class Person():
-    def __init__(self,name):
-        self.name = name
+    def info(self):
+        print(f"{self.owner}의 포켓몬은 입니다")
+        for skill in self.skills:
+            print(skill)
 
-class MDPerson(Person):
-    def __init__(self,name):
-        self.name = "Doctor" + name
+    def attack(self, idx):  #공격하는 클래스
+        print(f"{self.skills[idx]} 공격 시전!")
 
-class JDPerson(Person):
-    def __init__(self,name):
-        self.name=name + ", Esquire"
+
+class Pikachu(Pokemon):  # inheritance
+    def __init__(self, owner, skills):
+        super().__init__(owner, skills) #부모 클래스 호출
+        print(f"피카츄")
+
+class Ggoboogi(Pokemon): #inheritance
+    def __init__(self, owner, skills):
+        super().__init__(owner, skills) #부모 클래스를 상속 받는 함수 owner,skills 상속 받음
+        self.name = "꼬부기"
+        print(f"{self.name}")
+
+    def attack(self, idx):  #공격하는 클래스
+        print(f"{self.owner}의  {self.name} 가 {self.skills[idx]} 공격 시전!")
+
+    def swim(self):
+        print(f"{self.name}가 수영을 합니다.")
+
+
+pk1 = Pikachu('한지우', '번개/백만볼트')
+pk1.info()
+ggo1 = Ggoboogi("오바람", "고속스핀/거품/몸통박치기")
+ggo1.info()
+ggo1.attack(2)
+ggo1.swim()
+# p1 = Pokemon( '한지우', '50만 볼트/100만 볼트/번개')
+# p2 = Pokemon( '오바람', '고속스핀/거품/몸통박치기/하이드로펌프')
